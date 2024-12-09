0dfeabfa8b132e00e53e457949aaaca66683827b
gabriellebarboza12 <gabriellebarbozasenna@gmail.com>
2024-12-09 12:26:47 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,93 @@
+export default [
+  {
+    question: " Соmрutаdоrеѕ ѕãо máquіnаѕ ехtrаоrdіnárіаѕ, têm funçõеѕ muіtо аvаnçаdаѕ е іntеlіgеntеѕ. Маѕ раrа іѕѕо tudо funсіоnаr, реçаѕ е рrоgrаmаѕ ѕãо іndіѕреnѕávеіѕ. Ѕе fоrmоѕ соmраrаr um соmрutаdоr соm um ѕеr humаnо, quаl а реçа dо соmрutаdоr quе fаrіа а funçãо dо сérеbrо humаnо, fаlаndо еm аrmаzеnаgеm dе dаdоѕ?",
+
+    answers: [
+      { option: "HD", correct: true },
+      { option: "Processador", correct: false },
+      { option: "Monitor", correct: false },
+    ],
+  },
+  {
+    question: " Ѕе еu dіѕѕеr quе mеu соmрutаdоr dе mеѕа nãо еѕtá lіgаndо, ѕеndо quе еlе еѕtá соnесtаdо nа tоmаdа е о рrоblеmа nãо еѕtá nо mоnіtоr nеm nо bоtãо dе асіоnаmеntо. О рrоblеmа роdе еѕtаr а оndе?",
+    answers: [
+      { option: "Gabinete", correct: false },
+      { option: "Fonte", correct: true },
+      { option: "SATA", correct: false },
+    ],
+  },
+  {
+    question: " Quando falamos de HD interno ou externo, DVD, CD-ROM e disquete, queremos nos referir a?  ",
+    answers: [
+      { option: " Sistemas de edição de imagens, vídeos e/ou músicas.  ", correct: false },
+      { option: " Software de armazenamento ", correct: false },
+      { option: "Dispositivos de armazenamento", correct: true },
+    ],
+  },
+  {
+    question: "Qual é o cabo usado para conectar um monitor à placa-mãe ou placa de vídeo?   ",
+    answers: [
+      { option: " Cabo SATA ", correct: false },
+      { option: " Cabo RJ-45. ", correct: false },
+      { option: "  Cabo HDMI ", correct: true },
+    ],
+  },
+  {
+    question: " Marque a opção correta sobre a Placa-mãe:  ",
+    answers: [
+      { option: " Serve apenas para ordenar o computador", correct: false },
+      { option: " É localizada dentro do monitor, pois é ali que todos os dados são transmitidos. ", correct: false },
+      { option: "Ela interliga e conecta todos os componentes do computador  ", correct: true },
+    ],
+  },
+  {
+    question: " Por que não é indicado o fechamento do notebook logo após o desligamento do mesmo?  ",
+    answers: [
+      { option: " Pois ele ainda pode estar ligado e ao tentar ligar na próxima vez, ele não ligará mais.", correct: false },
+      { option: " Porque nos cabos que se encontram em seu interior, ainda estará passando energia, podendo assim, danificá-lo. ", correct: false },
+      { option: "Pois ele muito aquecido e ao fechá-lo, ele não conseguirá esfriar de maneira correta ", correct: true },
+    ],
+  },
+  {
+    question:" O que é necessário verificar antes de substituir a fonte de alimentação de um computador? ",
+    answers: [
+      { option: "  O tipo de processador instalado.", correct: false },
+      { option: " A potência e os conectores da nova fonte.", correct: true },
+      { option: "A versão do sistema operacional. ", correct: false},
+    ],
+  },
+{
+  question: " Qual ferramenta é usada para evitar danos aos componentes eletrônicos causados por eletricidade estática?  ",
+
+  answers: [
+    { option: " Pulseira antiestática.", correct: true },
+    { option: " Pinça de plástico.", correct: false },
+    { option: "Chave de fenda magnética.", correct: false },
+  ],
+},
+{
+  question: "Qual é a principal função da pasta térmica em um computador? ",
+
+  answers: [
+    { option: " Melhorar a transferência de calor entre o processador e o dissipador.", correct: true },
+    { option: " Aumentar a velocidade do processador.", correct: false },
+    { option: " Garantir o funcionamento da fonte de alimentação.", correct: false },
+  ],
+},
+{
+  question:" O que é necessário verificar antes de substituir a fonte de alimentação de um computador? ",
+  answers: [
+    { option: "  O tipo de processador instalado.", correct: false },
+    { option: " A potência e os conectores da nova fonte.", correct: true },
+    { option: "A versão do sistema operacional. ", correct: false},
+  ],
+},
+{
+  question:" Ao instalar um novo sistema operacional, qual é o primeiro passo recomendado?",
+  answers: [
+    { option: " Atualizar os drivers da placa de vídeo. ", correct: false },
+    { option: " Formatar o disco rígido ou SSD.", correct: true },
+    { option: " Configurar o monitor para resolução máxima", correct: false},
+  ],
+},
+];
